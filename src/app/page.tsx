ff95a8091c3ca99044745f2f0920d3d4d037c7ff
siyoon210 <siyoon210@gmail.com>
2024-09-29 13:50:22 +0900

    타이틀 설명 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,19 +9,22 @@ export default async function Page() {
           <h1 className="text-3xl font-bold">Pick the Teenieping</h1>
         </div>
         <div className="p-6 space-y-6">
+          <p className="text-center text-gray-600">
+            캐치! 티니핑 이미지 맞추기
+          </p>
           <div className="space-y-4">
             <Link
               href={"/quiz"}
               className="flex items-center justify-center w-full bg-black hover:bg-primary/90 text-white py-3 px-4 rounded-md transition-colors"
             >
-              <Play className="w-5 h-5 mr-2" />
+              <Play className="w-5 h-5 mr-2"/>
               게임 시작
             </Link>
             <Link
               href={"/ranking"}
               className="flex items-center justify-center w-full bg-gray-100 hover:bg-gray-100/90 text-secondary-foreground py-3 px-4 rounded-md transition-colors"
             >
-              <Trophy className="w-5 h-5 mr-2" />
+              <Trophy className="w-5 h-5 mr-2"/>
               랭킹 보기
             </Link>
           </div>
