f8fdbb381d99b4eeedf20d484addad3f519b5f76
siyoon210 <siyoon210@gmail.com>
2024-10-01 19:02:42 +0900

    og 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,16 @@ import { GoogleAnalytics } from '@next/third-parties/google'
 export const metadata: Metadata = {
   title: "캐치! 티니핑 이미지 퀴즈",
   description: "캐치! 티니핑 이미지 퀴즈",
+  openGraph: {
+    title: "캐치! 티니핑 이미지 퀴즈",
+    description: "캐치! 티니핑 이미지 퀴즈",
+    images: [
+      {
+        url: "/og-image.webp",
+        alt: "캐치! 티니핑 이미지 퀴즈",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({children,}: Readonly<{ children: React.ReactNode; }>) {
